01b179441de82cfe7e6a3dfd1fa545ef49076668
841148070 <841148070@qq.com>
2017-11-08 09:23:56 +0800

    first commit

---
+++ js/common.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+	$("#menu-click").click(function(){
+		$("#menu-total").slideToggle("slow");
+	});
+});
+
+
+
+$(document).ready(function(){
+	$("#KK").click(function(){
+		$(".KK").toggleClass("no");
+	});
+});
+
+
+$(document).ready(function(){
+	$("#SS").click(function(){
+		$(".SS").toggleClass("no");
+	});
+});
+
+
+$(document).ready(function(){
+	$("#ZZ").click(function(){
+		$(".ZZ").toggleClass("no");
+	});
+});
+
+
+$(document).ready(function(){
+	$("#st").click(function(){
+		$(".st").toggleClass("no");
+	});
+});
+
+
+$(document).ready(function(){
+	$("#dd").click(function(){
+		$(".dd").toggleClass("no");
+	});
+});
